2e158948b7b761dc5db0b92b7798a1bd6dfa2083
Ryan Liu <49974633+superR8014@users.noreply.github.com>
2023-10-03 13:35:52 +0800

    Delete example directory

--- example/index.tsx
+++
@@ -1,7 +0,0 @@
-import 'react-native-worklets-core/src';
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
-
-// @ts-ignore
-AppRegistry.registerComponent(appName, () => App);
